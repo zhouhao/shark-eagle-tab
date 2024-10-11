00c480f771c396b7c5eca7288c73a2cfb03f6a6b
Hao Zhou <hzhou.me@gmail.com>
2024-10-11 11:46:42 -0700

    :sparkles: enable entry delete and improve UI

--- src/options/App.vue
+++ src/options/App.vue
@@ -27,7 +27,10 @@
                 <strong class="mb-1">{{ group }}</strong>
                 <small><img :src="getImgSrc(group)" alt="favicon"/></small>
               </div>
-              <div class="col-10 mb-1 small">Url Count: {{ tabGroupUrlMap.get(group).length }} / Total View Count: {{ tabGroupViewCountMap.get(group) }}</div>
+              <div class="col-10 mb-1 small">
+                Url Count: {{ tabGroupUrlMap.get(group).length }} / Total View Count:
+                {{ tabGroupViewCountMap.get(group) }}
+              </div>
             </a>
           </div>
         </div>
@@ -48,8 +51,10 @@
             </div>
             <div class="card-body">
               <ol class="list-group list-group-numbered">
-                <li class="list-group-item" v-for="tab in currentTabList" :key="tab._id">
-                  <a :href="tab._id" target="_blank"> {{ tab.title }}</a> - <small>(Views: {{ tab.count }}, Last Viewed: {{ formatTime(tab.lastViewTime) }})</small>
+                <li class="list-group-item tab-entry" v-for="tab in currentTabList" :key="tab._id">
+                  <a :href="tab._id" target="_blank" :title="tab.title"> {{ truncateString(tab.title) }}</a> -
+                  <small>(Views: {{ tab.count }}, Last Viewed: {{ formatTime(tab.lastViewTime) }})</small>
+                  <button type="button" class="btn-close float-end delete-btn" aria-label="Close" @click="deleteNote(tab._id)"></button>
                 </li>
               </ol>
             </div>
@@ -153,6 +158,12 @@ export default {
     formatTime(ts) {
       return formatDatetime(ts);
     },
+    truncateString(str, maxLength = 60) {
+      if (str.length <= maxLength) {
+        return str;
+      }
+      return str.slice(0, maxLength) + '...';
+    },
   },
 };
 </script>
@@ -162,80 +173,15 @@ export default {
   max-width: 1440px;
 }
 
-.bd-placeholder-img {
-  font-size: 1.125rem;
-  text-anchor: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  user-select: none;
+.delete-btn {
+  color: red;
+  display: none;
+}
+.tab-entry:hover {
+  background-color: rgba(169, 169, 169, 0.15);
 }
 
-@media (min-width: 768px) {
-  .bd-placeholder-img-lg {
-    font-size: 3.5rem;
-  }
-}
-
-.b-example-divider {
-  width: 100%;
-  height: 3rem;
-  background-color: rgba(0, 0, 0, 0.1);
-  border: solid rgba(0, 0, 0, 0.15);
-  border-width: 1px 0;
-  box-shadow: inset 0 0.5em 1.5em rgba(0, 0, 0, 0.1), inset 0 0.125em 0.5em rgba(0, 0, 0, 0.15);
-}
-
-.b-example-vr {
-  flex-shrink: 0;
-  width: 1.5rem;
-  height: 100vh;
-}
-
-.bi {
-  vertical-align: -0.125em;
-  fill: currentColor;
-}
-
-.nav-scroller {
-  position: relative;
-  z-index: 2;
-  height: 2.75rem;
-  overflow-y: hidden;
-}
-
-.nav-scroller .nav {
-  display: flex;
-  flex-wrap: nowrap;
-  padding-bottom: 1rem;
-  margin-top: -1px;
-  overflow-x: auto;
-  text-align: center;
-  white-space: nowrap;
-  -webkit-overflow-scrolling: touch;
-}
-
-.btn-bd-primary {
-  --bd-violet-bg: #712cf9;
-  --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-  --bs-btn-font-weight: 600;
-  --bs-btn-color: var(--bs-white);
-  --bs-btn-bg: var(--bd-violet-bg);
-  --bs-btn-border-color: var(--bd-violet-bg);
-  --bs-btn-hover-color: var(--bs-white);
-  --bs-btn-hover-bg: #6528e0;
-  --bs-btn-hover-border-color: #6528e0;
-  --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-  --bs-btn-active-color: var(--bs-btn-hover-color);
-  --bs-btn-active-bg: #5a23c8;
-  --bs-btn-active-border-color: #5a23c8;
-}
-
-.bd-mode-toggle {
-  z-index: 1500;
-}
-
-.bd-mode-toggle .dropdown-menu .active .bi {
-  display: block !important;
+.tab-entry:hover .delete-btn {
+  display: block;
 }
 </style>
