efdf5a1244434baad29fe30b74588a5f57bf060f
SaltyEgg <hzhou.me@saltyee.com>
2024-10-19 14:01:12 -0700

    :bug: fix home page

--- src/options/components/Header.vue
+++ src/options/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <a href="/options/options.html" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <img class="logo" src="/icons/icon.png" alt="" width="40" />
         <span class="fs-4">Shark Eagle Tab</span>
       </a>
